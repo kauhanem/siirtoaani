2ab8a7c47a6bd0fd85812a14243ed78c474b2bb2
Mikko Kauhanen <mikko.kauhanen@tuni.fi>
2019-02-17 18:31:50 +0200

    all in a days work

--- namegen.py
+++ namegen.py
@@ -1,68 +1,90 @@
 import csv
 
-import numpy as numpy
+import numpy as np
 from scipy import stats
 
-def finngen(names,format="first sur",file=None):
+class nameGen:
+    def __init__(self,paths):
+        self.distributions = {
+            "sex" : self.createSexDist(paths["sex"]),
+            "first" : self.createFirstDist(paths["first"]),
+            "other" : self.createOtherDist(paths["other"]),
+            "sur" : self.createSurDist(paths["sur"]),
+            "bday" : self.createBDayDist(paths["bday"]),
+            "city" : self.createCityDist(paths["city"])
+        }
+
+    def generatePiece(self,piece,names):
+        return self.distributions[piece].rvs(size=names)
+    
+    
+    def createSexDist(self,path):
+        fl = []
+        f_names = []
+        f_probs = []
+        
+        total = 0
+        with open(path,'r',encoding='utf-8') as f:
+            for line in f:
+                values = line.split(',')
+                values[1] = int(values[1])
+                total += values[1]
+                fl.append(values)
+
+        for k in fl:
+            f_names.append(k[0])
+            k[1] = float(k[1]/total)
+            f_probs.append(k[1])
+        
+        xk = np.arange(len(f_names))
+
+        f.close()
+
+        return stats.rv_discrete(values=(xk,f_probs))
+
+    def createFirstDist(self):
+        
+
+def finngen(names,name_format="first sur",file=None):
     # lataa csv:t
-    paths = [
+    paths = {
         "miehet.csv",
         "naiset.csv",
         "kunnat.csv",
         "iät.csv"
-    ]
+    }
 
     firsts = []
     others = []
     surs = []
     cities = []
-
-    # määrittele funktiot
-    def sex():
-        s = sexes[s_d.rvs(size=1)]
-        return s
-    def name(s):
-        n = firsts[n_d.rvs(size=1)]
-        return n
-    def surname():
-        sn = surs[sr_d.rvs(size=1)]
-        return sn
-    def birthday(s):
-        return b
-    def idn(s,b):
-        return id
-    def city():
-        c = cities[c_d.rvs(size=1)]
-        return c
+    sexes = []
     
     # splittaa format-string
-    p = []
-    d = []
+    f_s = ""
+    d_s = ""
+    f_l = []
+    
+    for i in name_format:
+        if i.isalpha():
+            if d_s:
+                f_l.append(d_s)
+                d_s = ""
+            f_s += i
+        else:
+            if f_s:
+                f_l.append(f_s)
+                f_s = ""
+            d_s += i
+    if f_s:
+        f_l.append(f_s)
+    else:
+        f_l.append(d_s)
 
     # luo n nimeä loopissa
     full_names = []
-    for i in range(names):
-        s = sex()
-        b = birthday(s)
-
-        for j in range(len(p)):
-            string = ""
-            piece = p[j]
-            delim = d[j]
-
-            if piece == "name":
-                P = name(s)
-            elif piece == "surname":
-                P = surname()
-            elif piece == "birthday":
-                P = b
-            elif piece == "id":
-                P = idn(s,b)
-            elif piece == "city":
-                P = city()
-            
-            string = P + delim
-            full_names.append(string)
+    string = ""
+    full_names.append(string)
     
     # luo csv nimistä
     if file:
